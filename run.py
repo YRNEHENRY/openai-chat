f018a5aa58d92c84bb01cea2aa4f8361abe6c368
YRNEH <59800188+YRNEHENRY@users.noreply.github.com>
2023-03-31 11:09:45 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 import asyncio
 from config.requirements import *
-from config.openai_chat import *
-
 
 async def main():
     check_openai()
+    from config.openai_chat import *
     check_api_key()
     print("\nThe program will start, at any time enter 'quit' to stop.")
     await gpt3_chat()
